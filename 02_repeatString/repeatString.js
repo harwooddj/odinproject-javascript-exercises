ef68e495a0b331415c02b51302973fbb6d16b8b3
harwooddj <30828871+harwooddj@users.noreply.github.com>
2024-11-11 21:11:21 +0000

    first two exercises done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(text, number) {
+    let joinedText = '';
+    if(text === '' || number === 0  ){
+        return ''
+    }else if(number < 0){
+        return 'ERROR';
+    }else{
+        for(let i=0;i<number;i++){
+            joinedText+=text;
+        };
+        return joinedText;
+    };
 };
 
 // Do not edit below this line

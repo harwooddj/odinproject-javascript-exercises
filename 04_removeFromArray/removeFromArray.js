945304e4f8556e683df126c1596bce8f4b12edf3
harwooddj <30828871+harwooddj@users.noreply.github.com>
2024-11-17 18:01:41 +0000

    Completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    mainArr = arguments[0];
+    for(i=1;i<arguments.length;i++){
+        mainArr = mainArr.filter(x => x !== arguments[i]);
+    };
+    return mainArr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(...args) {
+    if (args.sort().every(arg => isNaN(arg))) {
+        return "ERROR";
+    }else{
+        total=args[0]
+        for(i=args[0];i<args[-1];i++){
+            total+=args[i];
+        };
+        return total;
+    };
 };
 
 // Do not edit below this line

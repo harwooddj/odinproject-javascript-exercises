512de303c82de6e92e8e8967ee1210c72486a49d
harwooddj <30828871+harwooddj@users.noreply.github.com>
2024-11-18 20:09:23 +0000

    completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	let total = nums.reduce((acc, curr) => {
+    return acc + curr
+  },0);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  let total = nums.reduce((acc, curr) => {
+    return acc * curr
+  },1);
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a,b)
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	result = 1
+  for(i=1; i<=n;i++){
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line

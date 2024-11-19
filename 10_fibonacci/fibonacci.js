b282820aad0bfd7c26b05a18c24004074268788b
harwooddj <30828871+harwooddj@users.noreply.github.com>
2024-11-19 15:03:27 +0000

    Completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+let fib = [0, 1];
+let fib2 = [0, 1];
+for(let i=fib.length; i<=num; i++) {
+    fib[i] = fib[i-2] + fib[i-1];
+    fib2.push(fib[i])
+}
+return fib2[num] > -1 ? fib2[num] : "OOPS";
 };
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;

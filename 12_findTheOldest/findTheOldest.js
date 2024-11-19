db7357bf0400a39f4d39141de12ec25ed6ceeb47
harwooddj <30828871+harwooddj@users.noreply.github.com>
2024-11-19 16:24:21 +0000

    Completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let lifespanArray = arr.map(person => {
+       return {name: person.name, lifespan: person.yearOfDeath ? person.yearOfDeath - person.yearOfBirth : new Date().getFullYear() - person.yearOfBirth}
+    })
+    let maxArray = lifespanArray.reduce((max, current) => max.lifespan > current.lifespan ? max : current)
+    console.log(lifespanArray, maxArray);
+    return maxArray;
 };
 
 // Do not edit below this line

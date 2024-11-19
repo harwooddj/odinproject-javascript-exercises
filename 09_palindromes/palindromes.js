e54c3f9c01d733b61be7aad18823b370312fe452
harwooddj <30828871+harwooddj@users.noreply.github.com>
2024-11-19 14:40:23 +0000

    completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+    forw = phrase.split('').filter(c => c.match(/^[a-zA-Z0-9]+$/) !== null).join().toLowerCase();
+    rev = phrase.split('').filter(c => c.match(/^[a-zA-Z0-9]+$/) !== null).reverse().join().toLowerCase();
+    return forw === rev;
 };
 
 // Do not edit below this line
